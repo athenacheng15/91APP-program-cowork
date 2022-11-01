201007a03e2d4139b334110964fea0654fcf5594
Emil Lau <r09343018@ntu.edu.tw>
2022-11-01 16:54:41 +0800

    Progress up to price

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -7,13 +7,18 @@ type PopUpProps={
 
 function PopUp({title,content,buttonText,buttonFunction}:PopUpProps) {
 	return ( 
-		<section className="bg-white fixed top-[45%] self-center w-[290px] h-[165px] rounded-md flex flex-col items-center justify-center">
-			<div className="text-[16px] font-[500] my-[18.5px]">{title}</div>
-			<div className="text-[13px] mb-[30px]">{content}</div>
-			<div className="w-[90%] h-[40px] cursor-pointer bg-[#fe494e] rounded-md flex items-center justify-center">
-				<div className="text-white text-[15px]">{buttonText}</div>
-			</div>
-		</section>
+		<>
+			<section className="w-[100%] h-[100%] bg-white opacity-0 fixed z-[2]"></section>
+			<section className="z-[2] bg-white fixed top-[45%] self-center w-[290px] h-[165px] rounded-md flex flex-col items-center justify-center">
+				<div className="text-[16px] font-[500] my-[18.5px]">{title}</div>
+				<div className="text-[13px] mb-[30px]">{content}</div>
+				<div 
+					onClick={()=>buttonFunction()}
+					className="w-[90%] h-[40px] cursor-pointer bg-[#fe494e] rounded-md flex items-center justify-center">
+					<div className="text-white text-[15px]">{buttonText}</div>
+				</div>
+			</section>
+		</>
 	// 改進：背景尚未調整
 	// <section className="fixed w-[100%] h-[120vh] bg-black opacity-25"></section> 
 	);
--- src/pages/register/Selection/SelectionWrapper.tsx
+++ src/pages/register/Selection/SelectionWrapper.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../../utili/useContext";
+import { useNavigate } from "react-router-dom";
 import Gallery from "./Gallery";
 import Selector from "./Selector";
 import TitlePrice from "./TitlePrice";
@@ -8,10 +9,12 @@ import defaultPhoto from"../../../img/register/iPhone-images/pro-iphone-13-sky.p
 import PopUp from "../../../components/PopUp";
 
 export default function Selection() {
-
+	const navigate = useNavigate();
 	const { regInfo } = useContext(UserContext);
 	const[image,setImage]=useState<string>(defaultPhoto);	
 	const[navBack,setNavBack]=useState<boolean>(false);
+	const [title,setTitle]=useState<string>("Apple iPhone 13");
+	const [price,setPrice]=useState<string>("");
 
 	useEffect(()=>{
 		//處理Gallery顯示的照片
@@ -26,6 +29,10 @@ export default function Selection() {
 	},[regInfo]);
 
 	useEffect(()=>{
+		regInfo.type.length!==0 && setTitle(`Apple ${regInfo.type}`);
+	},[regInfo]);
+
+	useEffect(()=>{
 		//如果未填寫基本資料即以url進入本頁，顯示彈窗並引導回上一頁
 		if(regInfo.name.length===0 || regInfo.phone.length===0 || regInfo.email.length===0){
 			console.log("wrong");
@@ -33,17 +40,18 @@ export default function Selection() {
 		}
 	},[]);
 
-	function test(){
-		console.log("hi");
-		
+	function navigateBack(){
+		navigate("/register/reg");
+		window.scroll({top: 0, left: 0, behavior: "smooth" }); 
 	}
 
 	return (
 		<section className="h-[max-content] bg-[#fafafa] flex flex-col">
 			<Gallery image={image}/>
-			<TitlePrice />
+			<TitlePrice title={title} price={price} />
 			<Selector />
-			{navBack&&(<PopUp title="個人資料有誤" content="個人資料尚未登記，請回上一頁填寫" buttonText="回上一頁" />)}
+			{/* {navBack&&(<PopUp title="個人資料有誤" content="個人資料尚未登記，請回上一頁填寫" buttonText="回上一頁" buttonFunction={navigateBack}/>)} */}
+			{/* 改進：之後記得將註解去除 */}
 		</section>
 
 
