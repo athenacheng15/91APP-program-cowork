06a0d93a3a933b11ebcfc72e51b9e6e603b62bab
Athena Cheng <athenacheng15@arch.nctu.edu.tw>
2022-11-03 17:27:04 +0800

    finish home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,24 @@
+import { useNavigate } from "react-router-dom";
 export default function Home() {
-	return <>Home</>;
+	const navigate = useNavigate();
+	return (
+		<div className="flex flex-wrap items-center justify-center w-[100vw] h-[100vh] bg-[#3c4048] ">
+			<div className="flex justify-center w-[100%] med:w-[50%]">
+				<button
+					className="w-[250px] h-[100px] text-[20px] bg-[#fff] border-4 border-solid border-black rounded-[20px] hover:text-[28px] hover:shadow-[10px_10px_0_0_rgb(0,0,0)] hover:border-[#fff] hover:text-[#665A48] hover:bg-[#FAD6A5] hover:translate-x-[-15px] hover:translate-y-[-15px] transition-all"
+					onClick={() => navigate("/products")}
+				>
+					Products
+				</button>
+			</div>
+			<div className=" flex justify-center w-[100%] med:w-[50%]">
+				<button
+					className="w-[250px] h-[100px] text-[20px] bg-[#fff] border-4 border-solid border-black rounded-[20px] hover:text-[28px] hover:shadow-[10px_10px_0_0_rgb(0,0,0)] hover:border-[#fff] hover:text-[#665A48] hover:bg-[#FAD6A5] hover:translate-x-[-15px] hover:translate-y-[-15px] transition-all"
+					onClick={() => navigate("/register")}
+				>
+					Registration
+				</button>
+			</div>
+		</div>
+	);
 }
