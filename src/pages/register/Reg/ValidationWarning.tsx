b1cbedec19f2f356903a672b97b6573d2e59f2e0
Emil Lau <r09343018@ntu.edu.tw>
2022-11-13 12:06:46 +0800

    fix warning ui

--- src/pages/register/Reg/ValidationWarning.tsx
+++ src/pages/register/Reg/ValidationWarning.tsx
@@ -5,7 +5,7 @@ type ValidationProps={
 
 function ValidationWarning({text,warning}:ValidationProps) {
 	return ( <>
-		<div className="text-[13px] text-[#ff5455] mt-[3px] w-[90%] self-center mb-[8px]">{text}</div>
+		<div className={`text-[13px] text-[#ff5455] mt-[3px] w-[90%] self-center mb-[8px] ${text.includes("手機")&&"md:self-start"}`}>{text}</div>
     
 	</> );
 }
