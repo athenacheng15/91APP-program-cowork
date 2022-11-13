41dd68bdb116f144ec1b0311956dc5464d89ab1f
Emil Lau <r09343018@ntu.edu.tw>
2022-11-13 18:49:51 +0800

    fix color picking bug

--- src/pages/register/Selection/Selector.tsx
+++ src/pages/register/Selection/Selector.tsx
@@ -6,18 +6,9 @@ import {simplised} from "../../../utili/chineseChanger";
 
 
 function Selector() {
-	const[colorList,setColorList]=useState<any>();
+
 	const {simplified} = useContext(UserContext);
-	useEffect(()=>{
-		if(simplified){
-			const newColorList=iPhoneColorList.map((item:any)=>({...item,name:simplised(item.name)}));
-			setColorList(newColorList);
-		}
-	},[simplified]);
 
-	
-
-	useEffect(()=>console.log(colorList),[colorList]);
 	
 	return ( <section className="w-[90%] self-center">
 		<div className="mb-[15px] text-[14px]">{simplified?simplised("選擇型號"):"選擇型號"}</div>
@@ -26,7 +17,7 @@ function Selector() {
 		</div>
 		<div className="mb-[5px] text-[14px] mt-[15px]">{simplified?simplised("選擇色彩"):"選擇色彩"}</div>
 		<div className="flex flex-wrap">
-			<EachSelector info={simplified&&colorList!==undefined?colorList:iPhoneColorList} localType="color"/>
+			<EachSelector info={iPhoneColorList} localType="color"/>
 		</div>
 		<div className="mb-[15px] text-[14px] mt-[15px]">{simplified?simplised("選擇容量"):"選擇容量"}</div>
 		<div className="flex mb-[15px]">
